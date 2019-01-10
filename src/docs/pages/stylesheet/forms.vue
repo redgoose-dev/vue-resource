5066a27a7fdad05b54eadc046c3af01c5cbb3f7c
redgoose <scripter@me.com>
2019-01-10 19:27:16 +0900

    forms 부분 작성중, web-forms 문서 작성

--- src/docs/pages/stylesheet/forms.vue
+++ src/docs/pages/stylesheet/forms.vue
@@ -2,9 +2,39 @@
 <article class="page">
 	<head-title type="h2" title="forms"/>
 
+	<blockquote>
+		<p>
+			<strong>Source: <a href="https://github.com/redgoose-dev/vue-redgoose-source/blob/master/src/css/forms.scss" target="_blank">forms.scss</a></strong>
+		</p>
+	</blockquote>
+
 	<p>
-		폼에 관련된 스타일시트
+		<code>forms.scss</code> 파일에 들어있는 스타일시트들은 폼 관련 컴포넌트들을 그루핑하거나 보조하는 스타일들을 작성해서 관리한다.<br/>
+		예를들어 <code>폼 항목의 필드, 필드셋, 폼에대한 도움말 및 오류 메시지..</code> 같은 부분들에서 사용하는 스타일시트를 만들어 사용한다.
 	</p>
+	<p>
+		프로젝트에 따라 많은 조정이 필요한 부분이므로 스타일시트로만 만들어 사용하는것이 좋아 보인다.
+	</p>
+	<p>
+		현재 파일에서는 다른 클래스와의 이름 구분을 위하여 <code>.form-</code>이라는 프리픽스를 사용한다.
+	</p>
+
+	<head-title type="h3" title="fieldset"/>
+	<p>
+		...
+	</p>
+
+	<head-title type="h3" title="field"/>
+	<p>
+		...
+	</p>
+
+	<head-title type="h3" title="폼 관련 컴포넌트 목록"/>
+	<p>다음은 입력폼에서 사용하는 컴포넌트들이다.</p>
+	<ul>
+		<li><router-link to="/">text</router-link></li>
+		<li><router-link to="/">textarea</router-link></li>
+	</ul>
 </article>
 </template>
 
--- src/docs/pages/stylesheet/web-fonts.vue
+++ src/docs/pages/stylesheet/web-fonts.vue
@@ -8,12 +8,22 @@
 		</p>
 	</blockquote>
 
-	<!--<section>-->
-		<!--<head-title type="h3" title="web-fonts"/>-->
-	<!--</section>-->
 	<p>
-		사용자 폰트 불러오기 가이드 // TODO: 작성예정
+		서비스에서 클라이언트 시스템에 없는 새로운 글꼴을 사용할때 웹폰트를 이용한다.<br/>
+		딱히 제대로된 리소스는 적어놓을것들은 없을거 같고, 글꼴을 불러오는데 사용하는 예제로 <a href="https://fonts.google.com/specimen/Roboto" target="_blank">Roboto</a>, <a href="https://material.io/tools/icons/?style=baseline" target="_blank">Material design icons</a> 글꼴을 불러오는 예제정도로 기록해 두었다.
 	</p>
+
+	<p>
+		<code>@font-face</code>를 통하여 웹폰트를 불러오는 초기화할때 반복되는 코드를 줄이고 싶으면 <code>mixin</code>을 사용하면 많은 도움이 된다.<br/>
+		사용법은 소스에서 `Roboto`폰트를 불러오는 부분에서 참고할 수 있다.
+	</p>
+
+	<head-title type="h3" title="참고 사이트"/>
+	<ul>
+		<li><a href="https://fonts.google.com/specimen/Roboto" target="_blank">Roboto</a></li>
+		<li><a href="https://material.io/tools/icons/?style=baseline" target="_blank">Material design icons</a></li>
+		<li><a href="https://spoqa.github.io/spoqa-han-sans/ko-KR/" target="_blank">스포카 한 산스</a></li>
+	</ul>
 </article>
 </template>
 
