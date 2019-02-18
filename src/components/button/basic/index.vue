188e7c39c694bc8ed074f88f7e99f04cdba5de72
redgoose <scripter@me.com>
2019-02-18 19:39:25 +0900

    버튼 컴포넌트 작업중..

--- src/components/button/basic/index.vue
+++ src/components/button/basic/index.vue
@@ -1,3 +1,91 @@
 <template>
-	<div>.buttons/basic</div>
+<router-link
+	v-if="to"
+	:to="to"
+	:title="title"
+	:class="classNames"
+	@click.native="onClick">
+	<span class="button__wrap">
+		<slot/>
+	</span>
+</router-link>
+<a
+	v-else-if="href"
+	:href="href"
+	:target="target"
+	:title="title"
+	:class="classNames"
+	@click="onClick">
+	<span class="button__wrap">
+		<slot/>
+	</span>
+</a>
+<button
+	v-else
+	:type="type"
+	:title="title"
+	:class="classNames"
+	@click="onClick">
+	<span class="button__wrap">
+		<slot/>
+	</span>
+</button>
 </template>
+
+<script>
+export default {
+	name: 'button-basic',
+	props: {
+		to: { type: String, default: null },
+		href: { type: String, default: null },
+		target: { type: String, default: null },
+		type: { type: String, default: 'button' }, // button,reset,submit
+		title: { type: String, default: null },
+		size: { type: String, default: null }, // small,large
+		color: { type: String, default: null }, // none,sub,gray
+		round: { type: Boolean, default: false }, // true,false
+		disabled: { type: Boolean, default: false }, // true,false
+		outline: { type: Boolean, default: false }, // true,false
+		inline: { type: Boolean, default: false }, // true,false
+	},
+	computed: {
+		buttonType()
+		{
+			if (!!this.to)
+			{
+				return 'router';
+			}
+			else if (!!this.href)
+			{
+				return 'a';
+			}
+			else
+			{
+				return 'button';
+			}
+		},
+		classNames()
+		{
+			return [
+				'button',
+				`button--type-${this.buttonType}`,
+				this.size && `button--size-${this.size}`,
+				this.color && `button--size-${this.color}`,
+				this.round && 'button--round',
+				this.disabled && 'button--disabled',
+				this.outline && 'button--outline',
+				this.inline && 'button--inline',
+			];
+		}
+	},
+	methods: {
+		onClick(e)
+		{
+			e.preventDefault();
+			// TODO: router-link 에서 클릭 이벤트 막는법 찾아보기
+			this.$emit('click', e);
+		}
+	},
+}
+</script>
+<style src="./index.scss" lang="scss" scoped></style>
--- src/docs/pages/components/button/basic/index.vue
+++ src/docs/pages/components/button/basic/index.vue
@@ -1,6 +1,22 @@
 <template>
 <article class="page">
 	<head-title type="h2" title="button - basic"/>
+
+	<blockquote>
+		<p>
+			<strong>Source: <a href="https://github.com/redgoose-dev/vue-resource/tree/master/src/components/button/basic" target="_blank">button/basic</a></strong>
+		</p>
+	</blockquote>
+
+	<p>
+		기본 디자인의 버튼 컴포넌트입니다.
+	</p>
+
+	<section>
+		<div class="example">
+			<button-basic to="/" @click="foo">sdgsdg</button-basic>
+		</div>
+	</section>
 </article>
 </template>
 
@@ -9,6 +25,13 @@ export default {
 	name: 'page',
 	components: {
 		'head-title': () => import('@/docs/component/title'),
+		'button-basic': () => import('@/components/button/basic'),
 	},
+	methods: {
+		foo(e)
+		{
+			console.log(e, 'foooo');
+		}
+	}
 }
 </script>
