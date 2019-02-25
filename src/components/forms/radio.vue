c571a07f23c671ff8085e2f44ea7fef0e06c92b4
redgoose <scripter@me.com>
2019-02-25 14:10:09 +0900

    radio 컴포넌트 작업중..

---
+++ src/components/forms/radio.vue
@@ -0,0 +1,53 @@
+<template>
+<label
+	class="form-radio"
+	:class="[
+		!!color && `form-radio--color-${color}`,
+		!!size && `form-radio--size-${size}`,
+		!!animation && `form-radio--animation-${animation}`,
+		!!disabled && `form-radio--disabled`,
+	]">
+	<input
+		type="radio"
+		:name="name"
+		:id="id"
+		:value="value"
+		:checked="checked"
+		:disabled="disabled"
+		:required="required"
+		@change="onChange"/>
+	<span>
+		<i>icon</i>
+		<em v-if="!!label">{{label}}</em>
+	</span>
+</label>
+</template>
+
+<script>
+export default {
+	name: 'form-radio',
+	props: {
+		name: { type: String, default: null },
+		id: { type: String, default: null },
+		label: { type: String, default: null },
+		value: { type: [String,Number,Boolean] },
+		checked: { type: [Boolean, String], default: false },
+		disabled: { type: Boolean, default: false },
+		required: { type: Boolean, default: false },
+		color: { type: String, default: null }, // sub,error
+		size: { type: String, default: null }, // small,large
+	},
+	model: {
+		prop: 'checked',
+		event: 'change',
+	},
+	methods: {
+		onChange(e)
+		{
+			this.$emit('change', e);
+		},
+	},
+}
+</script>
+
+<style src="./radio.scss" lang="scss" scoped></style>
--- src/docs/pages/components/form/radio.vue
+++ src/docs/pages/components/form/radio.vue
@@ -1,6 +1,43 @@
 <template>
 <article class="page">
 	<head-title type="h2" title="form / radio"/>
+
+	<blockquote>
+		<p>
+			<strong>
+				unit component: <a href="https://github.com/redgoose-dev/vue-resource/blob/master/src/components/forms/radio.vue" target="_blank">radio.vue</a><br/>
+				group component: <a href="https://github.com/redgoose-dev/vue-resource/blob/master/src/components/forms/radios.vue" target="_blank">radios.vue</a>
+			</strong>
+		</p>
+	</blockquote>
+
+	<p>여러 항목들 중에 하나를 선택하는 컴포넌트 입니다.</p>
+
+	<section>
+		<head-title type="h3" title="basic"/>
+		<p>기본적인 형태로 사용합니다.</p>
+		<div class="example">
+			<form-radio name="foo" label="label-name"/>
+			<form-radio name="foo" label="label-name"/>
+			<form-radio name="foo" label="label-name"/>
+		</div>
+	</section>
+
+	<section>
+		<head-title type="h3" title="size"/>
+	</section>
+
+	<section>
+		<head-title type="h3" title="color"/>
+	</section>
+
+	<section>
+		<head-title type="h3" title="disabled"/>
+	</section>
+
+	<section>
+		<head-title type="h3" title="animation"/>
+	</section>
 </article>
 </template>
 
@@ -9,6 +46,7 @@ export default {
 	name: 'page',
 	components: {
 		'head-title': () => import('@/docs/layout/title'),
+		'form-radio': () => import('@/components/forms/radio'),
 	},
 }
 </script>
