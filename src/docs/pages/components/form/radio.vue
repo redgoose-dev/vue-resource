45318574d3d618d4adfae99f92a3a9d5d2c35fe1
redgoose <scripter@me.com>
2019-03-07 18:53:41 +0900

    radio 컴포넌트 문서에 props 항목 추가

--- src/docs/pages/components/form/radio.vue
+++ src/docs/pages/components/form/radio.vue
@@ -117,6 +117,72 @@
 // none
 &lt;form-radio animation="none"/></code></pre>
 	</section>
+
+	<section>
+		<head-title type="h3" title="props"/>
+		<p>이 컴포넌트는 다음과 같은 <code>props</code>를 사용합니다.</p>
+		<table class="table table--center table--bordered">
+			<thead>
+			<tr>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Comment</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td>name</td>
+				<td>String</td>
+				<td class="typo-align--left"><code>name</code> attribute 로 사용</td>
+			</tr>
+			<tr>
+				<td>id</td>
+				<td>String</td>
+				<td class="typo-align--left"><code>id</code> attribute 로 사용</td>
+			</tr>
+			<tr>
+				<td>label</td>
+				<td>String</td>
+				<td class="typo-align--left">항목에 표시되는 이름입니다. 값이 없으면 라디오 폼만 나타납니다.</td>
+			</tr>
+			<tr>
+				<td>value</td>
+				<td>String,Number,Boolean</td>
+				<td class="typo-align--left"><code>value</code> attribute 로 사용</td>
+			</tr>
+			<tr>
+				<td>checked</td>
+				<td>String,Boolean</td>
+				<td class="typo-align--left">체크되어있는지의 여부</td>
+			</tr>
+			<tr>
+				<td>disabled</td>
+				<td>Boolean</td>
+				<td class="typo-align--left">비활성화</td>
+			</tr>
+			<tr>
+				<td>required</td>
+				<td>Boolean</td>
+				<td class="typo-align--left">필수값인지에 대한 여부</td>
+			</tr>
+			<tr>
+				<td>color</td>
+				<td>String</td>
+				<td class="typo-align--left">컴포넌트의 색상 <code>sub,success,error</code></td>
+			</tr>
+			<tr>
+				<td>size</td>
+				<td>String</td>
+				<td class="typo-align--left">컴포넌트의 사이즈 <code>small,large</code></td>
+			</tr>
+			<tr>
+				<td>animation</td>
+				<td>String</td>
+				<td class="typo-align--left">체크할때의 애니메이션 방식 <code>elastic,none</code></td>
+			</tr>
+			</tbody>
+		</table>
+	</section>
 </article>
 </template>
 
